f477379e56a050738eb69f7cccb3c3ec1bf5eb8f
Wavefire5201 <1541053496ee@gmail.com>
2024-12-14 03:35:08 -0600

    add profile cards to about section, fix some styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -11,6 +13,9 @@ export default defineConfig({
     react(),
     tailwind({
       applyBaseStyles: false,
+    }),
+    icon({
+      iconDir: "src/assets/icons",
     }),
   ],
 });
