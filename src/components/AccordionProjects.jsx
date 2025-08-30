8056b2828745838986b096ad2ecf84a7d3f2bc5c
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2025-08-29 22:03:52 -0500

    chore: edit project description

--- src/components/AccordionProjects.jsx
+++ src/components/AccordionProjects.jsx
@@ -48,6 +48,10 @@ const AccordionProjects = () => (
       <AccordionContent>
         {/* // description */}
         <div className="py-4">
+          A modern farmhouse concept designed for sustainable family living.
+          This 2-bedroom, 2-bathroom home features a spacious layout, including
+          a large garage and patio, that thoughtfully integrates classic design
+          with modern, eco-friendly efficiencies and materials.
           <Accordion.Root
             className="AccordionRoot"
             type="multiple"
